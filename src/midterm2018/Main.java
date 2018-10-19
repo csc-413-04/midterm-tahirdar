c08fd7a8a7463b02fbce31ce3e548741780733bb
tahirdar <tahirdar@mail.sfsu.edu>
2018-10-18 21:47:48 -0700

    Completed

--- src/midterm2018/Main.java
+++ src/midterm2018/Main.java
@@ -1,5 +1,9 @@
 package midterm2018;
 
+import com.google.gson.*;
+import com.google.gson.JsonObject;
+import java.lang.reflect.Field;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -12,33 +16,53 @@ public class Main {
             + "Accept-Encoding: gzip, deflate, br\n"
             + "Accept-Language: en-US,en;q=0.9,es;q=0.8\n";
 
-        String question3 = "{\n"
-            + "    “task” : “inc”,\n"
-            + "    “num” : 3\n"
-            + "}\n";
+        String question3 = "{"
+                    + "    “task” : “inc”,"
+                    + "    “num” : 3"
+        + "}";
 
         String question4and5 = "To opt out, you’ll need to enter the Settings menu by clicking the three vertical dots, all the way in the upper right corner of the browser. From there, you’ll need to enter the Advanced settings at the very bottom and find the “Allow Chrome sign in” toggle, then turn it to off. Doing so lets you sign into Google services like Gmail and Maps without signing into the Chrome browser itself.";
 
 
-        System.out.println(question1and2);
-        System.out.println(question3);
-        System.out.println(question4and5);
+        //System.out.println(question1and2);
+        //System.out.println(question3);
+        //System.out.println(question4and5);
 
         // print each answer at the end
 
         // Question 1
         // Return the Host
+        int n1 = question1and2.indexOf("Host:");
+        int n2 = question1and2.indexOf("Connection:");
+        String q1 = question1and2.substring(n1 + 6, n2);
+        System.out.println(q1);
+
 
         // Question 2
         // return sum of a and b
+        int n3 = question1and2.indexOf("a=");
+        int n4 = question1and2.indexOf(" ", n3);
 
+        String t = question1and2.substring(n3, n4).replace("&", " ");
+        String [] t2 = t.split(" ");
+        int val1 = Integer.parseInt((t2[0].split("=")[1]));
+        int val2 = Integer.parseInt((t2[1].split("=")[1]));
+        System.out.println("sum of a + b is " + (val1 + val2)) ;
+
+
+        //
         // Question 3
         // convert to java object, increment num, convert back to json and return
-
-        // Question 4
-        // return unique words
+        Gson gson = new Gson();
+        JsonObject object = gson.fromJson(question3, JsonObject.class);
+        int temp = Integer.parseInt(object.get("num").getAsString()) + 1; // Incrementing by 1
+        object.addProperty("num", temp);  //Updated the property
+        System.out.println(object.toString());
 
         // Question 5
         // return 2nd most common word
+        //
+        //
+        //
     }
 }
